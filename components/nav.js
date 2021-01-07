5ce35d8acbb01b0bd08cd9400e466eb2d7db7db4
miladgera014@gmail.com <miladgera014@gmail.com>
2021-01-07 15:19:30 +0330

    change menu in nav

--- components/nav.js
+++ components/nav.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Link from "next/link";
 
-const links = [
-  { href: "https://zeit.co/now", label: "ZEIT" },
-  { href: "https://github.com/zeit/next.js", label: "GitHub" },
-].map((link) => {
-  link.key = `nav-link-${link.href}-${link.label}`;
-  return link;
-});
+// const links = [
+//   { href: "https://zeit.co/now", label: "ZEIT" },
+//   { href: "https://github.com/zeit/next.js", label: "GitHub" },
+// ].map((link) => {
+//   link.key = `nav-link-${link.href}-${link.label}`;
+//   return link;
+// });
 
 const Nav = () => (
   <nav className="text-center">
@@ -17,13 +17,18 @@ const Nav = () => (
           <a className="text-blue-500 no-underline text-sm">Home</a>
         </Link>
       </li>
-      {links.map(({ key, href, label }) => (
+      <li className="flex px-2 py-1">
+        <Link href="/about">
+          <a className="text-red-700 no-underline text-sm">About</a>
+        </Link>
+      </li>
+      {/* {links.map(({ key, href, label }) => (
         <li key={key} className="flex px-2 py-1">
           <a className="text-blue-500 no-underline text-sm" href={href}>
             {label}
           </a>
         </li>
-      ))}
+      ))} */}
     </ul>
   </nav>
 );
